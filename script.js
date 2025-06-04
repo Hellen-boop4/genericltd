b536c633aba85e9160579913425f42ef9398a79b
Hellen-boop4 <124364001+Hellen-boop4@users.noreply.github.com>
2025-06-04 13:50:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) {
+      slide.classList.add('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Show the first slide initially
+showSlide(currentSlide);
+
+// Change slide every 5 seconds
+setInterval(nextSlide, 5000);
